00b8f7ae16b126e25f224b941440a0f2a252e5f5
HvqEMAOU <hvqemaou@email.com>
2021-04-15 15:21:57 +0000

    Update Manager.java

--- src/main/java/com/epam/test/automation/java/practice6/Manager.java
+++ src/main/java/com/epam/test/automation/java/practice6/Manager.java
@@ -1,9 +1,33 @@
 package com.epam.test.automation.java.practice6;
 
-/**
- * <summary>
- * Implement code according to description of task.
- * </summary>
- */
-public class Manager {
+import java.math.BigDecimal;
+
+public class Manager extends Employee {
+	   
+	    private int quantity;
+
+	    public Manager (String name, BigDecimal salary, int clientAmount) {
+	        super (name, salary);
+	        this.quantity = clientAmount;
+	    }
+
+	    @Override
+	    public void setBonus(BigDecimal bonus) {
+	    	if (bonus == null || bonus.compareTo(BigDecimal.ZERO) < 0)
+	    		throw new IllegalArgumentException("bonus is null or < 0");
+
+	    	BigDecimal bd500 = new BigDecimal(500);
+	    	BigDecimal bd1000 = new BigDecimal("1000");
+	    	
+	    	if (quantity>100 && quantity<=150) {	 	
+	    		BigDecimal bonusOver100 = bonus.add(bd500);
+	    		this.bonus = (bonusOver100);
+	    	}
+	    	else if (quantity>150) {
+	    		BigDecimal bonusOver150 = bonus.add(bd1000);
+	    		this.bonus = (bonusOver150);
+	    	}
+	    	else 
+	    		this.bonus = bonus;
+	    }
 }
